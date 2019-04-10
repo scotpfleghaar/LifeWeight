cb3fbbb833d3d535e79650c732d812dcd762901a
scot.pfleghaar <scot.pfleghaar@gmail.com>
2019-04-10 18:04:37 -0500

    Actions

---
+++ src/Redux/Actions/AuthenticationActions.js
@@ -0,0 +1,50 @@
+import {
+    EMAIL_CHANGED,
+    PASSWORD_CHANGED,
+    LOGIN_USER_SUCCESS,
+    LOGIN_USER_FAILED,
+    LOGIN_USER_INITIALIZED
+} from "../../../Constants";
+import firebase from 'firebase';
+
+export const emailChange = (text) => {
+    return {
+        type: EMAIL_CHANGED,
+        payload: text
+    }
+};
+
+export const passwordChange = (text) => {
+    return {
+        type: PASSWORD_CHANGED,
+        payload: text
+    }
+};
+
+export const loginUser = ({email, password}) => (dispatch) => {
+    dispatch({
+        type: LOGIN_USER_INITIALIZED
+    });
+    firebase.auth().signInWithEmailAndPassword(email, password)
+        .then(user => loginUserSuccess(dispatch, user))
+        .catch(() => {
+            firebase.auth().createUserWithEmailAndPassword(email, password)
+                .then(user => loginUserSuccess(dispatch, user))
+                .catch(() => loginUserFail(dispatch))
+        });
+};
+
+const loginUserSuccess = (dispatch, user) => {
+    dispatch({
+        type: LOGIN_USER_SUCCESS,
+        payload: user
+    });
+   // @TODO Handle Navigation
+};
+
+const loginUserFail = (dispatch) => {
+    return dispatch({
+        type: LOGIN_USER_FAILED,
+        payload: "Authentication Failed"
+    })
+};
