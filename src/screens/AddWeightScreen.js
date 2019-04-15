b92df27ea7fa6f43f81f8be6772efe9f484a64b4
scot.pfleghaar <scot.pfleghaar@gmail.com>
2019-04-14 20:43:07 -0500

    Began working on the weight input screen

--- src/screens/AddWeightScreen.js
+++ src/screens/AddWeightScreen.js
@@ -1,26 +1,85 @@
 import React, {Component} from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {View, StyleSheet, Dimensions} from 'react-native';
+import {Input} from 'react-native-elements'
+import {HANSIS_MEDIUM_DARK, HANSIS_MEDIUM} from "../../Constants";
+import Icon from 'react-native-vector-icons/FontAwesome';
+import {HeaderWrapper, FormButton} from './Components'
+import {connect} from "react-redux";
+
+const DEVICE_WIDTH = Dimensions.get('window').width;
+
 
 class AddWeightScreen extends Component {
+    state;
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            weight: ''
+        }
+    }
+
+    goBack() {
+        console.log('Button Pressed');
+        this.props.navigation.navigate.pop()
+    };
+
     render() {
         return (
-            <View style={styles.container}>
-                <Text>
-                    AddWeightScreen
-                </Text>
-            </View>
+            <HeaderWrapper
+                title={'Add Record'}
+            >
+                <View style={styles.formStyle}>
+                    <Input
+                        placeholder='Weight'
+                        keyboardType='numeric'
+                        value={this.state.weight}
+                        onChangeText={(text) => {
+                            this.setState({
+                                weight: text
+                            })
+                        }}
+                        leftIconContainerStyle={styles.iconContainerStyle}
+                        inputContainerStyle={{height: 52}}
+                    />
+                    <FormButton
+                        title={"Add Entry"}
+                        onPress={() => this.goBack.bind(this)}
+                    />
+                </View>
+            </HeaderWrapper>
         );
     }
 }
 
-
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
+    iconContainerStyle: {
+        width: 52
+    },
+
+    headerStyle: {
+        height: 64,
+        textAlign: 'center',
+        borderBottomColor: HANSIS_MEDIUM,
+        borderBottomWidth: 1,
+        width: DEVICE_WIDTH,
+    },
+
+    formStyle: {
+        height: 240,
         justifyContent: 'center',
+        alignItems: 'center',
     }
 });
 
-export default AddWeightScreen;
+const mapStateToProps = state => {
+    const {email, password, error, loading} = state.auth;
+    return {
+        email,
+        password,
+        error,
+        loading
+    }
+};
+
+export default connect(mapStateToProps, {})(AddWeightScreen);
