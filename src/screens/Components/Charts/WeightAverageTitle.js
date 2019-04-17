a83092008a11e2c4dfd5566ac02015d8542bebf4
Scot Pfleghaar <scot.pfleghaar@gmail.com>
2019-04-17 13:58:12 +0000

    tenDayAverage title in place

--- src/screens/Components/Charts/WeightAverageTitle.js
+++ src/screens/Components/Charts/WeightAverageTitle.js
@@ -1,15 +1,18 @@
 import React, {Component} from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { mean } from 'lodash'
+import { HANSIS_DARK } from '../../../../Constants'
 
 class WeightAverageTitle extends Component {
     render() {
-        const adjustedRecords = this.props.records.slice(0, 16);
+        const adjustedRecords = this.props.records.slice(0, 11);
         const data = adjustedRecords.map(item => item.weight && item.weight);
+        const tenDayAverage = mean(data).toFixed(1)
 
         return (
             <View style={styles.container}>
-                <Text>
-                    WeightAverageTitle
+                <Text style={styles.textStyle}>
+                    { tenDayAverage }
                 </Text>
             </View>
         );
@@ -21,6 +24,10 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    textStyle: {
+        fontSize: 40,
+        color: HANSIS_DARK
     }
 });
 
