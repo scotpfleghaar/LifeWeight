eab4ebecf586866598bee2ee6fcdb8689ba7f3de
scot.pfleghaar <scot.pfleghaar@gmail.com>
2019-04-16 21:27:26 -0500

    Charts working

--- src/screens/Components/Charts/WeightLineChart.js
+++ src/screens/Components/Charts/WeightLineChart.js
@@ -2,14 +2,17 @@ import React from 'react'
 import { AreaChart } from 'react-native-svg-charts'
 import * as shape from 'd3-shape'
 import { Circle } from 'react-native-svg'
-import { HANSIS_DARK } from '../../../../Constants'
+import {HANSIS_DARK, HANSIS_MEDIUM_LIGHT } from '../../../../Constants'
+import { userGageToColor } from '../../Utilities'
 
 class WeightLineChart extends React.PureComponent {
 
     render() {
 
-        const data = [ 50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80 ]
+        // const data = [ 50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80 ]
+        const data = this.props.records.map(item => item.weight && item.weight);
 
+        console.log(data);
         return (
             <AreaChart
                 style={ { height: 200 } }
@@ -24,11 +27,11 @@ class WeightLineChart extends React.PureComponent {
                            cy={ y(value) }
                            r={ 4 }
                            stroke={ HANSIS_DARK }
-                           fill={ 'white' }
+                           fill={ userGageToColor(this.props.records[index].userWeightGage) }
                        />
                    )
                 }}
-                svg={{ fill: 'rgba(134, 65, 244, 0.8)' }}
+                svg={{ fill: HANSIS_MEDIUM_LIGHT }}
             />
         )
     }
