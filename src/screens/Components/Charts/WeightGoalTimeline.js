9e1e74eda245f7036b34bb4699c30c15a7eca30d
Scot Pfleghaar <scot.pfleghaar@gmail.com>
2019-04-18 18:26:19 +0000

    Weight loss goal data

--- src/screens/Components/Charts/WeightGoalTimeline.js
+++ src/screens/Components/Charts/WeightGoalTimeline.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { ProgressCircle } from 'react-native-svg-charts'
 import { Text, StyleSheet, View } from 'react-native'
-import { HANSIS_DARK, HANSIS_MEDIUM_DARK } from '../../../../Constants'
+import { HANSIS_DARK, HANSIS_MEDIUM_DARK, MONTHS } from '../../../../Constants'
 import { connect } from 'react-redux'
 import { sortRecords, weightLoseRate } from '../../Utilities'
 import { mean } from 'lodash'
@@ -14,32 +14,18 @@ class WeightGoalTimeline extends Component {
         const goalWeight = 165;
 
         const averageRate = mean(weightLoseRate(this.props.records));
-
         const data = this.props.records.map(item => item.weight && Number(item.weight));
-
         const tenDayAverageWeight = mean(data).toFixed(1)
-
-        console.log('NUMBERS:', averageRate, goalWeight, tenDayAverageWeight)
-
-        const daysUntilComplete = ((tenDayAverageWeight - goalWeight) /averageRate).toFixed(0)
-        console.log('daysUntilComplete', daysUntilComplete, typeof daysUntilComplete)
+        const daysUntilComplete = ((tenDayAverageWeight - goalWeight) / averageRate).toFixed(0)
         let now = new Date(); 
         now.setDate(now.getDate() + Number(daysUntilComplete));
-        console.log(now)
-        console.log(`${now.getMonth() + 1}, ${now.getDate()}, ${now.getFullYear()}`)
+
         return (
             <View>
-                <ProgressCircle
-                    style={ { height: 200 } }
-                    progress={ 0.7 }
-                    progressColor={ HANSIS_MEDIUM_DARK }
-                    startAngle={ -Math.PI * 0.8 }
-                    endAngle={ Math.PI * 0.8 }
-                />
-
-                <Text style={styles.textStyle}>Current Rate: { averageRate }</Text>
-                <Text style={styles.textStyle}>Current Weight: { tenDayAverageWeight }</Text>
-                <Text style={styles.textStyle}>Goal Complition: { `${now.getMonth() + 1} ${now.getDate()}, ${now.getFullYear()}` }</Text>
+                <Text>Weight loss rate per week:</Text>
+                <Text style={styles.textStyle}>{ `${(averageRate * 7).toFixed(2)}` }</Text>
+                <Text>At your current rate you'll reach your goal on:</Text>
+                <Text style={styles.textStyle}>{ `${MONTHS[now.getMonth()]} ${now.getDate()}, ${now.getFullYear()}` }</Text>
             </View>
         )
     }
