b43c0c2693fd919f173c35c9f4666ba326815798
scot.pfleghaar <scot.pfleghaar@gmail.com>
2019-04-24 19:45:32 -0500

    bumped Version

--- src/screens/ExtrasScreen.js
+++ src/screens/ExtrasScreen.js
@@ -11,7 +11,7 @@ class ExtrasScreen extends Component {
             >
                 <View style={styles.container}>
                     <Text>
-                        LifeWeight provides general information health and related subjects. The information and other content provided by LifeWeight, are not intended and should not be construed as medical advice, nor is the information a substitute for professional medical expertise or treatment.
+                        LifeWeight provides general information about health and related subjects. The information and other content provided by LifeWeight, are not intended and should not be construed as medical advice, nor is the information a substitute for professional medical expertise or treatment.
                         If you or any other person has a medical concern, you should consult with your health care provider or seek other professional medical treatment. If you think you may have a medical emergency, call your doctor or emergency services immediately.
                         The information provided by LifeWeight have no relation to those of any academic, hospital, health practice or other institution.
                     </Text>
