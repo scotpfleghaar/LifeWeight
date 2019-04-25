44c0eac49a514d1495647432dae472ff4e105013
scot.pfleghaar <scot.pfleghaar@gmail.com>
2019-04-24 19:05:56 -0500

    Styling the the hint

--- src/screens/Components/HowWellDidYouFollowYourDietDescription.js
+++ src/screens/Components/HowWellDidYouFollowYourDietDescription.js
@@ -2,12 +2,13 @@ import React, {Component} from 'react';
 import {Button, Overlay, Text, Divider, Icon} from 'react-native-elements'
 import {Dimensions, StyleSheet, View, Keyboard} from 'react-native'
 import {HANSIS_MEDIUM, HANSIS_MEDIUM_LIGHT, HANSIS_MEDIUM_DARK, PURE_WHITE, HANSIS_DARK} from "../../../Constants";
+
 const DEVICE_HEIGHT = Dimensions.get('window').height;
 const DEVICE_WIDTH = Dimensions.get('window').width;
 
 class HowWellDidYouFollowYourDietDescription extends Component {
     render() {
-         const happyIcon = () => <Icon
+        const happyIcon = () => <Icon
             type={'font-awesome'}
             name={'check'}
             color={'green'}
@@ -25,49 +26,55 @@ class HowWellDidYouFollowYourDietDescription extends Component {
 
 
         return (
-          <View style={styles.containerStyle}>
-                <Text h4>Description:</Text>
-                <Divider/>
-               <Text>How well did you follow your diet since you last checked in?</Text>
+            <View style={styles.containerStyle}>
+                <Text h4>Hint:</Text>
+                <Divider style={styles.dividerStyle}/>
+                <Text>This is a measure if how well did you follow your diet since you last checked in.</Text>
+                <Divider style={styles.dividerStyle}/>
                 <View style={styles.textWrapperStyle}>
-                        {happyIcon()}<Text style={styles.textStyle}>Nailed it!</Text>
+                    {happyIcon()}<Text style={styles.textStyle}>Yes!</Text>
                 </View>
                 <View style={styles.textWrapperStyle}>
-                        {sortaIcon()}<Text style={styles.textStyle}>Moderately</Text>
+                    {sortaIcon()}<Text style={styles.textStyle}>Yes and No</Text>
                 </View>
                 <View style={styles.textWrapperStyle}>
-                        {sadIcon()}<Text style={styles.textStyle}>did not follow</Text>
+                    {sadIcon()}<Text style={styles.textStyle}>No</Text>
                 </View>
-                <Text>This helps determine helpful and motivational information for you.</Text>
-                <Text>This information gets generated into graphs that give you insight into what happens when you follow your diet!</Text>
-        </View>
+                <Divider style={styles.dividerStyle}/>
+                <Text>Its best to be honest with yourself, because this information determines helpful and motivational information for you.</Text>
+                <Text>This information gets generated into graphs that give you insight into what happens when you
+                    follow your diet!</Text>
+            </View>
         );
     }
 }
 
 
-
 const styles = StyleSheet.create({
-        textWrapperStyle: {
-            flexDirection: 'row',
-             justifyContent: 'space-between',
-             alignItems: 'center',
-             width: "auto",
-             height: 30,
-             margin: 10
-        },
+    textWrapperStyle: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: "auto",
+        height: 30,
+        margin: 10
+    },
 
-        textStyle: {
-            fontSize: 18
-        },
+    textStyle: {
+        fontSize: 18
+    },
 
-        containerStyle: {
-            margin: 10,
-             flexDirection: 'column',
-             justifyContent: 'space-between',
-        }
+    dividerStyle: {
+        marginBottom: 10,
+        marginTop: 10
+    },
+
+    containerStyle: {
+        margin: 10,
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+    }
 });
-
 
 
 export default HowWellDidYouFollowYourDietDescription;
